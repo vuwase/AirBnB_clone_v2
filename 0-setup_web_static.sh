eddcfc1bcb704cabdf5dca55ff331e40fd39fab3
vuwase <v.uwase@alustudent.com>
2024-02-16 11:11:38 -0800

    updated

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,38 +1,19 @@
-#!/usr/bin/env bash
-# Sets up a web server for deployment of web_static.
+#!/usr/bin/python3
+"""Comment"""
+from fabric.api import local
+from datetime import datetime
 
-apt-get update
-apt-get install -y nginx
 
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+def do_pack():
+    """Comment again"""
+    local("mkdir -p versions")
 
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive_path = "versions/web_static_{}.tgz".format(timestamp)
 
-printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root   /var/www/html;
-    index  index.html index.htm;
+    result = local("tar -cvzf {} web_static".format(archive_path))
 
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-
-    location /redirect_me {
-        return 301 http://cuberule.com/;
-    }
-
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}" > /etc/nginx/sites-available/default
-
-service nginx restart
+    if result.failed:
+        return None
+    else:
+        return archive_path
