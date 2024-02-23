97e20d29e114044108098e5f970e845ddc602205
vuwase <v.uwase@alustudent.com>
2024-02-23 12:01:15 -0800

    done

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,35 +1,43 @@
 #!/usr/bin/python3
+"""second task"""
+from flask import Flask, render_template
 
-"""Script that starts a Flask web application"""
-from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """Comment"""
+    """first page"""
     return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """Comment"""
+    """second page"""
     return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def text_route(text):
-    """Comment"""
-    return "C {}".format(text.replace("_", " "))
+@app.route("/c/<text>", strict_slashes=False)
+def C_fun(text):
+    """C page"""
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
 
 
-@app.route('/python', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def text_route_python(text="is cool"):
-    """Comment"""
-    return "Python {}".format(text.replace("_", " "))
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def Python_fun(text="is cool"):
+    """ Python page"""
 
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def numbers(n):
+    """Number page"""
+    return '{} is a number'.format(n)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
