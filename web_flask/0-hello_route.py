bd14ffbca29acc5729de25e38843387586e38f40
M-Pascal <p.mugisha@alustudent.com>
2024-02-23 20:07:00 +0000

    frame work

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
-
-"""Script that starts a Flask web application"""
+"""Script that starts a Flask web application."""
 from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """Comment"""
+def hello():
     return "Hello HBNB!"
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
